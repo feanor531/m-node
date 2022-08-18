60b1d7c24403efca70beb8eef2cb6045ebf2870c
feanor531 <feanor531@gmail.com>
2022-08-18 13:36:10 +0300

    add validatePassword and create tests

--- L1.1/validator.js
+++ L1.1/validator.js
@@ -10,36 +10,71 @@ let Validator = {
     },
 
     validatePassword(pass) {
-
+        const regexp = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)\w{8,}$/;
+        return regexp.test(pass);
     },
 };
 
-// let mail = ["fi@secondpart.end",
-//     "first-part@.se=cond%p.art.end",
-//     "first.part@se=cond%part.r",
-//     "first-partkfkfkfkfkfk@.se=cond%p.art.end",
-//     "f@secondart.end,",
-//     "first-part@.se=cond@part.end",
-//     "-firstpart@.se=cond%.enddeded",
-//     "firs_tpart@.se.en",
-//     "firstpart@.se.enddeded",
-//     "_first.part@se=cond%part.r",
-//     "+first.part@se=cond%part.r"];
+console.log("\nvalidateEmail test\n");
 
-// mail.forEach(element => {
-//     console.log(element + " : "  + Validator.validateEmail(element));
-// });    
+let mail = {
+    "fi@secondpart.end" : true,
+    "first-part@.se=cond%p.art.end" : true,
+    "first.part@se=cond%part.r" : true,
+    "first-partkfkfkfkfkfk@.se=cond%p.art.end" : false,
+    "f@secondart.end," : false,
+    "first-part@.se=cond@part.end" : false,
+    "-firstpart@.se=cond%.enddeded" : false,
+    "firs_tpart@.se.en" : false,
+    "firstpart@.se.enddeded" : false,
+    "_first.part@se=cond%part.r" : false,
+    "+first.part@se=cond%part.r" : false,
+};
 
-let phone = [
-    '+38 (099) 567                                                   8901',
-    '+38 099 5 6 7 8 9  01',
-    '(09-9) 567-890-1',
-    '--  (099) 567 890-1',
-    '+38 (099) 567 8901 0',
-    '+38 099 a0000000',
-    '+38 (0989) 567 8901',
-    '+48 (0989) 567 8901',
-];
-phone.forEach(element => {
-    console.log(element + " : "  + Validator.validatePhone(element));
-}); 
+for (const key in mail) {
+    if (Object.hasOwnProperty.call(mail, key)) {
+        console.log((Validator.validateEmail(key) === mail[key] ? "Pass" : "Fail") + " : "  + key);       
+    }
+}
+
+console.log("\nvalidatePhone test\n");
+
+let phone = {
+    '+38 (099) 567                                                   8901' : false,
+    '+38 (099) 567 8901' : true,
+    '+38 099 5 6 7 8 9  01' : true,
+    '(09-9) 567-890-1' : true,
+    '--  (099) 567 890-1' : true,
+    '+38 (099) 567 8901 0' : false,
+    '+38 099 a0000000' : false,
+    '+38 (0989) 567 8901' : false,
+    '+48 099) 567 8901' : false,
+};
+
+for (const key in phone) {
+    if (Object.hasOwnProperty.call(phone, key)) {
+        console.log((Validator.validatePhone(key) === phone[key] ? "Pass" : "Fail") + " : "  + key);       
+    }
+}
+
+console.log("\nalidatePassword test\n");
+
+let pass = {
+    'C00l_Pass' : true,
+    '10SupperPas' : true,
+    'SupperPas1' : true,
+    'Cool_pass' : false,
+    'cool_password' : false,
+    'C00l_Pa' : false,
+    'COOOOOOOL111' : false,
+    'C00l_Pass+' : false,
+    'C00l' : false,
+    'COOl_Pass' : false,
+};
+
+
+for (const key in pass) {
+    if (Object.hasOwnProperty.call(pass, key)) {
+        console.log((Validator.validatePassword(key) === pass[key] ? "Pass" : "Fail") + " : " + key);        
+    }
+}
